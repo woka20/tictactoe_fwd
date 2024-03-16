c46cc754806ff54e0b5ca3935720e6499af8d35a
Woka Aditama <woka@ptmitrabhakti.com>
2024-03-16 13:01:58 +0700

    controller post dynamic board size

--- tictactoe/src/main/java/com/woka/tictactoe/controller/TictacToeController.java
+++ tictactoe/src/main/java/com/woka/tictactoe/controller/TictacToeController.java
@@ -1,16 +1,25 @@
 package com.woka.tictactoe.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.woka.tictactoe.service.TicTacToeService;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 @Controller
 public class TictacToeController {
 
+    @Autowired
+    private TicTacToeService tacToeService;
+    
     private char[][] board;
     private char currentPlayer;
     private int boardSize;
@@ -21,6 +30,15 @@ public class TictacToeController {
         model.addAttribute("board", board);
         return "index";
     }
+
+    @PostMapping("/generate-board")
+    public String generateBoard(@RequestParam int size, Model model) {
+        board=tacToeService.generateBoard(size); 
+        model.addAttribute("board", board);
+        return "index";   
+        
+    }
+    
 
  
 }
--- tictactoe/src/main/java/com/woka/tictactoe/service/TicTacToeService.java
+++ tictactoe/src/main/java/com/woka/tictactoe/service/TicTacToeService.java
@@ -1,5 +1,20 @@
 package com.woka.tictactoe.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TicTacToeService {
+
+    private char [][] boards;
+
+    public char[][] generateBoard(int size){
+        for(int i=0; i<size; i++){
+            for(int j =0 ; j<size; j++){
+                boards[i][j]='\00';
+            }
+        }
+        return boards;
+
+    }
     
 }
